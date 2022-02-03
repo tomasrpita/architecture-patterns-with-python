ae6d0a3e57fe3c1b1cc15b853c35fb8c6bb9d1d7
Tomás R. Pita <tomasrpita@gmail.com>
2022-02-03 06:26:34 +0100

    Adds pysicopg2 to requirements.in and fix config.py typos

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
 
-def get_postgres_url():
-    host = os.everiron.get("POSTGRES_HOST", "localhost")
-    port = 54321 if host == "localhost" else 5432
-    password = os.environ.get("POSTGRES_PASSWORD", "tu-nunca-lo-sabras")
+def get_postgres_uri():
+    host = os.environ.get("POSTGRES_HOST", "localhost")
+    # port = 54321 if host == "localhost" else 5432
+    port = 5432 if host == "localhost" else 5432
+    password = os.environ.get("POSTGRES_PASSWORD", "abc123")
     user, db_name = "allocation", "allocation"
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
@@ -13,4 +14,3 @@ def get_api_url():
     host = os.environ.get("API_HOST", "localhost")
     port = 5000 if host == "localhost" else 80
     return f"http://{host}:{port}"
-    
