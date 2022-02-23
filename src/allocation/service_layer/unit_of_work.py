84967c9b6ccdfea44f2b24bec45ad9f31a43a9ad
Tomás R. Pita <tomasrpita@gmail.com>
2022-02-23 12:36:34 +0100

    Don’t Mock What You Don’t Own

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -1,11 +1,15 @@
 import abc
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import adapters.repository as repository
+import config
 
 class AbstractUnitOfWork(abc.ABC):
     batches: repository.AbstractRepository
 
     def __exit__(self, *args):
-        self.rollback() 
+        self.rollback()
 
     @abc.abstractmethod
     def commit(self):
@@ -15,3 +19,36 @@ class AbstractUnitOfWork(abc.ABC):
     @abc.abstractmethod
     def rollback(self):
         raise NotImplementedError
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        config.get_postgres_uri()
+    )
+)
+
+
+class SqlAlchemnyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+        self.session = None
+        self.committed = False
+
+    def commit(self):
+        self.committed = True
+        self.session.commit()
+
+    def rollback(self):
+        if self.committed:
+            self.session.rollback()
+        # else:
+            self.session.close()
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.batches = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,8 +1,8 @@
-from flask import session
 import pytest
-import adapters.repository as repository
-import domain.model as model
-import service_layer.services as services 
+import src.allocation.adapters.repository as repository
+import src.allocation.domain.model as model
+import src.allocation.service_layer.services as services
+import src.allocation.service_layer.unit_of_work as unit_of_work
 
 
 class FakeSession:
@@ -27,42 +27,91 @@ class FakeRepository(repository.AbstractRepository):
         return list(self._batches)
 
 
-def test_returns_allocation():
-    repo = FakeRepository([])
-    session = FakeSession()
-    services.add_batch("batch-1", "sku-1", 10, "2011-01-01", repo, session)
-    
-    result = services.allocate("order-1", "sku-1", 10, repo, session)
-    
-    assert result == "batch-1"
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    def __init__(self):
+        self.batches = FakeRepository([])
+        self.committed = False
+
+    def commit(self):
+        self.commited = True
+
+    def rollback(self):
+        pass
+
+
+# def test_returns_allocation():
+#     repo = FakeRepository([])
+#     session = FakeSession()
+#     services.add_batch("batch-1", "sku-1", 10, "2011-01-01", repo, session)
+
+#     result = services.allocate("order-1", "sku-1", 10, repo, session)
+
+#     assert result == "batch-1"
+
+
+# def test_error_for_invalid_sku():
+#     repo = FakeRepository([])
+#     session = FakeSession()
+
+#     services.add_batch("batch-1", "sku-2", 100, "2011-01-01", repo, session)
+
+#     with pytest.raises(services.InvalidSku, match="Invalid sku sku-1"):
+#         services.allocate("order-1", "sku-1", 10, repo, FakeSession())
+
+
+# def test_add_batch():
+#     repo, session = FakeRepository([]), FakeSession()
+#     services.add_batch("batch-1", "sku-1", 100, "2011-01-01", repo, session)
+
+#     assert repo.get("batch-1") is not None
+#     assert session.commited is True
+
+
+# def test_commit():
+
+#     repo = FakeRepository([])
+#     session = FakeSession()
+#     session2 = FakeSession()
+
+#     services.add_batch("batch-1", "sku-1", 100, "2011-01-01", repo, session)
+#     services.allocate("order-1", "sku-1", 10, repo, session2)
+
+#     assert session.commited is True
+#     assert session2.commited is True
+
+
+# Test Unit Of Work
+
+def test_add_batch():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+    assert uow.batches.get("b1") is not None
+    assert uow.committed
+
+
+def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    assert result == "batch1"
 
 
 def test_error_for_invalid_sku():
-    repo = FakeRepository([])
-    session = FakeSession()
-    
-    services.add_batch("batch-1", "sku-2", 100, "2011-01-01", repo, session)
+    uow = FakeUnitOfWork()
+
+    services.add_batch("batch-1", "sku-2", 100, "2011-01-01", uow)
 
     with pytest.raises(services.InvalidSku, match="Invalid sku sku-1"):
-        services.allocate("order-1", "sku-1", 10, repo, FakeSession())
-
-
-def test_add_batch():
-    repo, session = FakeRepository([]), FakeSession()
-    services.add_batch("batch-1", "sku-1", 100, "2011-01-01", repo, session)
-
-    assert repo.get("batch-1") is not None
-    assert session.commited is True
+        services.allocate("order-1", "sku-1", 10, uow)
 
 
 def test_commit():
 
-    repo = FakeRepository([])
-    session = FakeSession()
-    session2 = FakeSession()
-    
-    services.add_batch("batch-1", "sku-1", 100, "2011-01-01", repo, session)
-    services.allocate("order-1", "sku-1", 10, repo, session2)
-    
-    assert session.commited is True
-    assert session2.commited is True
+    uow = FakeUnitOfWork()
+    another_uow = FakeUnitOfWork()
+
+    services.add_batch("batch-1", "sku-1", 100, "2011-01-01", uow)
+    services.allocate("order-1", "sku-1", 10, another_uow)
+
+    assert uow.commited is True
+    assert another_uow.commited is True
