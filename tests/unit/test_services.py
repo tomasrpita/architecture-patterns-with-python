cd0290850b69f1a7e569d8585c8021b9db41fd2c
tomasrpita <tomasrpita@gmail.com>
2022-03-04 16:24:13 +0100

    test integration

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -61,31 +61,34 @@ def test_add_batch_for_new_product():
     assert uow.committed
 
 
-# def test_add_batch_for_existing_product():
+def test_add_batch_for_existing_product():
+    uow = FakeUnitOfWork()
+    
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+    services.add_batch("b2", "CRUNCHY-ARMCHAIR", 99, None, uow)
 
-# def test_allocate_returns_allocation():
-
-# def test_allocate_errors_for_invalid_sku():
-
-# def test_allocate_commits():
+    assert "b2" in [b.reference for b in uow.products.get("CRUNCHY-ARMCHAIR").batches]
 
 
+def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
 
-# def test_allocate_returns_allocation():
-#     uow = FakeUnitOfWork()
-#     services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
-#     result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
-#     assert result == "batch1"
+    result = services.allocate("ord-1", "COMPLICATED-LAMP", 10, uow)
 
-# def test_allocate_error_for_invalid_sku():
-#     uow = FakeUnitOfWork()
-#     services.add_batch("batch-1", "sku-2", 100, "2011-01-01", uow)
+    assert "batch1" == result
 
-#     with pytest.raises(services.InvalidSku, match="Invalid sku sku-1"):
-#         services.allocate("order-1", "sku-1", 10, uow)
 
-# def test_allocate_commits():
-#     uow = FakeUnitOfWork()
-#     services.add_batch("batch-1", "sku-1", 100, "2011-01-01", uow)
-#     services.allocate("order-1", "sku-1", 10, uow)
-#     assert uow.committed is True
+def test_allocate_errors_for_invalid_sku():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch-1", "sku-2", 100, "2011-01-01", uow)
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku sku-1"):
+        services.allocate("order-1", "sku-1", 10, uow)
+
+
+def test_allocate_commits():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch-1", "sku-1", 100, "2011-01-01", uow)
+    services.allocate("order-1", "sku-1", 10, uow)
+    assert uow.committed is True
