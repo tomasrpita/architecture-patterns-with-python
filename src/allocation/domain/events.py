09585b8715a6d8ca81e04b65e3fbf989bbd96d93
Tomás R. Pita <tomasrrpita@gmail.com>
2022-03-29 14:33:31 +0200

    Changing the code accord the chapter

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Optional
+from datetime import date
 
 class Event:
 	pass
@@ -6,3 +8,16 @@ class Event:
 @dataclass
 class OutOfStock(Event):
 	sku: str
+
+@dataclass
+class BatchCreated(Event):
+	ref: str
+	sku: str
+	qty: int
+	eta: Optional[date] = None
+
+@dataclass
+class AllocationRequired(Event):
+	order_id: str
+	sku: str
+	qty: int
