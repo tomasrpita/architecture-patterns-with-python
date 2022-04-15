d7d431f4814fbdc9226e01af6efe4cd2506383e7
Tomás R. Pita <tomasrpita@gmail.com>
2022-04-15 16:39:34 +0200

    Test-Driving It All Using an End-to-End Test

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -16,12 +16,12 @@ app = Flask(__name__)
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     try:
-        event = commands.Allocate(
+        cmd = commands.Allocate(
             request.json["orderid"],
             request.json["sku"],
             request.json["qty"],
         )
-        results = messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
+        results = messagebus.handle(cmd, unit_of_work.SqlAlchemyUnitOfWork())
         batchref = results.pop(0)
 
     except (handlers.InvalidSku) as e:
@@ -37,13 +37,13 @@ def add_batch_endpoint():
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
 
-    event = commands.CreateBatch(
+    cmd = commands.CreateBatch(
         request.json["batchref"],
         request.json["sku"],
         request.json["qty"],
         eta,
     )
 
-    messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
+    messagebus.handle(cmd, unit_of_work.SqlAlchemyUnitOfWork())
 
     return {"message": "ok"}, 201
