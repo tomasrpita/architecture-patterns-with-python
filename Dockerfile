51f3e9ecb77978e1186d416387e786495f6bf413
Tomás R. Pita <tomasrpita@gmail.com>
2022-05-15 12:38:37 +0200

    Make Docker works

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ COPY requirements.txt /tmp/
 RUN pip install -r /tmp/requirements.txt
 
 RUN mkdir -p /src
-RUN mkdir -p /tests
 COPY src/ /src/
+RUN pip install -e /src
 COPY tests/ /tests/
 
 WORKDIR /src
--- src/allocation/config.py
+++ src/allocation/config.py
@@ -1,25 +1,47 @@
 import os
 
 
+# def get_postgres_uri():
+#     host = os.environ.get("POSTGRES_HOST", "localhost")
+#     # port = 54321 if host == "localhost" else 5432
+#     port = 5432 if host == "localhost" else 5432
+#     password = os.environ.get("POSTGRES_PASSWORD", "abc123")
+#     user, db_name = "allocation", "allocation"
+#     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+# def get_api_url():
+#     # host = os.environ.get("API_HOST", "localhost")
+#     # port = 5000 if host == "localhost" else 80
+#     host = os.environ.get("API_HOST", "127.0.0.1")
+#     port = 5000 if host == "127.0.0.1" else 80
+#     return f"http://{host}:{port}"
+
+
+# def get_redis_host_and_port():
+#     host = os.environ.get("REDIS_HOST", "127.0.0.1")
+#     port = 63791 if host == "127.0.0.1" else 6379
+#     return dict(host=host, port=port)
+
+
+import os
+
+
 def get_postgres_uri():
-    host = os.environ.get("POSTGRES_HOST", "localhost")
-    # port = 54321 if host == "localhost" else 5432
-    port = 5432 if host == "localhost" else 5432
-    password = os.environ.get("POSTGRES_PASSWORD", "abc123")
+    host = os.environ.get("DB_HOST", "localhost")
+    port = 54321 if host == "localhost" else 5432
+    password = os.environ.get("DB_PASSWORD", "abc123")
     user, db_name = "allocation", "allocation"
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
 def get_api_url():
-    # host = os.environ.get("API_HOST", "localhost")
-    # port = 5000 if host == "localhost" else 80
-    host = os.environ.get("API_HOST", "127.0.0.1")
-    port = 5000 if host == "127.0.0.1" else 80
+    host = os.environ.get("API_HOST", "localhost")
+    port = 5005 if host == "localhost" else 80
     return f"http://{host}:{port}"
 
 
 def get_redis_host_and_port():
-    host = os.environ.get("REDIS_HOST", "127.0.0.1")
-    port = 63791 if host == "127.0.0.1" else 6379
+    host = os.environ.get("REDIS_HOST", "localhost")
+    port = 63791 if host == "localhost" else 6379
     return dict(host=host, port=port)
-
