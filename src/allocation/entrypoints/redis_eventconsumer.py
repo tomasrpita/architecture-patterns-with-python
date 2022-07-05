963a26055c0ce9089cc4feeb075564f11b1a3698
Tomás R. Pita <tomasrrpita@gmail.com>
2022-07-05 14:50:19 +0200

    Using Bootstrap in Our Entrypoints

--- src/allocation/entrypoints/redis_eventconsumer.py
+++ src/allocation/entrypoints/redis_eventconsumer.py
@@ -8,14 +8,16 @@ from allocation.adapters import orm
 from allocation.domain import commands
 from allocation.service_layer import messagebus
 from allocation.service_layer import unit_of_work
+from allocation import bootstrap
 
 logger = logging.getLogger(__name__)
 
 r = redis.Redis(**config.get_redis_host_and_port())
 
+bus =bootstrap.bootstrap()
 
 def main():
-    orm.start_mappers()
+    # orm.start_mappers()
     pubsub = r.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe("change_batch_quantity")
 
@@ -27,7 +29,8 @@ def handle_change_batch_quantity(m):
     logging.debug("handling %s", m)
     data = json.loads(m["data"])
     cmd = commands.ChangeBatchQuantity(ref=data["batchref"], qty=data["qty"])
-    messagebus.handle(cmd, uow=unit_of_work.SqlAlchemyUnitOfWork())
+    # messagebus.handle(cmd, uow=unit_of_work.SqlAlchemyUnitOfWork())
+    bus.handle(cmd)
 
 
 if __name__ == "__main__":
